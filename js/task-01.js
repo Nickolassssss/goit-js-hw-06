390c1959eb866eec40f58be7fa16e084475ef894
Nickolassssss <mukolaohremchyk22@gmail.com>
2022-02-02 22:44:16 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,9 @@
-const categoriesEl = document.querySelector('#categories');
-const itemsEl = [...categoriesEl.children];
 
-console.log(`В списке ${itemsEl.length} категории.`);
-
-itemsEl.map(item => {
- console.log(`Категория: ${item.firstElementChild.textContent}`);
- console.log(`Количество элементов: ${item.lastElementChild.children.length}`);
+const list = document.querySelector('#categories');
+const items = list.querySelectorAll('.item');
+console.log(`В списке ${items.length} категории:`);
+items.forEach(node => {
+const h2 = node.querySelector('h2');
+const nodeItems = node.querySelectorAll('li');
+console.log(` - категория: ${h2.textContent} (количество элементов: ${nodeItems.length})`);
 });
